3576e8959f7ee9db46212d8d259daac1409cc91c
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2023-07-23 23:11:01 +0100

    done

--- public/projects.js
+++ public/projects.js
@@ -4,7 +4,7 @@ const projects = [
     name: "GadgetTrac",
     description:
       "GadgetTrac is a cutting-edge Device and Gadget Sales Management System that empowers businesses to efficiently track, manage, and optimize the entire sales lifecycle of gadgets. From inventory management to order processing and sales analytics, GadgetTrac provides real-time insights and seamless automation, streamlining operations and boosting sales performance. With its user-friendly interface and powerful features, GadgetTrac ensures businesses stay ahead in the fast-paced world of gadget sales.",
-    image: "/src/assets/2023-07-22.png",
+    image: "https://i.ibb.co/G7thD1W/2023-07-22.png",
     link: "https://gadgettrac.netlify.app",
     github: "https://github.com/Emerald211/Sales-Mangement-System",
   },
@@ -13,7 +13,7 @@ const projects = [
     name: "CircleChat",
     description:
       "CircleChat is an immersive group chatting application designed to foster seamless communication and collaboration among users. With its intuitive interface and user-friendly features, CircleChat brings people together in dynamic virtual circles, enabling real-time discussions, idea sharing, and bonding over shared interests. From casual conversations to productive teamwork, CircleChat delivers an engaging platform for meaningful interactions. Emphasizing simplicity and efficiency, this web application demonstrates my expertise in creating inclusive and impactful digital environments for enhanced connectivity.",
-    image: "/src/assets/CHAT.png",
+    image: "https://i.ibb.co/fNqZbZH/CHAT.png",
     link: "https://circlechat.netlify.app",
     github: "https://github.com/Emerald211/CHATAPP",
   },
@@ -22,7 +22,7 @@ const projects = [
     name: "Lillies Food App",
     description:
       "A food e-commerce project showcasing a user-friendly web app with extensive product catalog, personalized recommendations, order tracking, and responsive design. Built using cutting-edge technologies for seamless compatibility across devices. Aims to provide delightful shopping experience for food enthusiasts and health-conscious customers.",
-    image: "/src/assets/LILLIES FOOD.png",
+    image: "https://i.ibb.co/nQMQ7Vg/LILLIES-FOOD.png",
     link: "https://lilliesfoods.netlify.app",
     github: "https://github.com/Emerald211/LILLIES-FOOD-APP",
   },
@@ -32,7 +32,7 @@ const projects = [
     name: "JokeBotica",
     description:
       "JokeBotica is an engaging web application and fun project features a clever Joke-telling robot that delivers random jokes with a delightful twist. Through seamless text-to-speech integration, users can enjoy a lighthearted and entertaining experience as they listen to the jokes. JokeBotica aims to spread laughter and joy while demonstrating my skills in web development and audio integration.",
-    image: "/src/assets/JOKE TELLER.png",
+    image: "https://i.ibb.co/Qdh8sBW/JOKE-TELLER.png",
     link: "https://emerald211.github.io/JOKE-TELLER",
     github: "https://github.com/Emerald211/JOKE-TELLER",
   },
